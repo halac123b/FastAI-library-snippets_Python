3b696f28569417d2ab1741ea69db133232b75a18
halac123b <halac123b@gmail.com>
2024-06-28 17:57:04 +0700

    Update m_1: Modify import way, add cmt

--- 1/cat_dog_classification.py
+++ 1/cat_dog_classification.py
@@ -1,4 +1,4 @@
-from fastai.vision.all import *
+import fastai.vision.all as vision
 
 # fastai’s applications all use the same basic steps and code:
 # 1. Create appropriate DataLoaders
@@ -12,23 +12,28 @@ def is_cat(x):
 
 
 # Download file model from preset URL of fastai
-path = untar_data(URLs.PETS) / "images"
-# Create DataLoaders
-dls = ImageDataLoaders.from_name_func(
-    path,
-    get_image_files(path),
-    valid_pct=0.2,
-    seed=42,
+## untar_data(): download file from URL
+## URLs: class chứa rất nhiều data và model có sẵn của FastAI
+path = vision.untar_data(vision.URLs.PETS) / "images"
+
+# Create DataLoaders: Basic wrapper around several DataLoaders with factory methods for computer vision problems
+## Factory method: 1 design pattern - đóng gói sự phức tạp khi phải quản lí cùng lúc nhiều Dataloader (mỗi instance phụ trách 1 dataset và task khác nhau)
+## from_name_func(): tạo Dataloader từ path đến dataset và hàm label
+dls = vision.ImageDataLoaders.from_name_func(
+    path,  # path đến dataset
+    vision.get_image_files(path),  # Get tất cả image từ path dataset
+    valid_pct=0.2,  # Sử dụng 20% dataset dùng cho bước validation
+    seed=42,  # Seed cho các lệnh random
     label_func=is_cat,
-    item_tfms=Resize(224),
+    item_tfms=vision.Resize(224),
 )
 
 # Create a Learner
-learn = vision_learner(dls, resnet34, metrics=error_rate)
+learn = vision.vision_learner(dls, vision.resnet34, metrics=vision.error_rate)
 
 # Call a fit method
 learn.fine_tune(1)
-img = PILImage.create("images/cat.jpg")
+img = vision.PILImage.create("images/cat.jpg")
 
 # Make predictions and view results
 is_cat, _, probs = learn.predict(img)
